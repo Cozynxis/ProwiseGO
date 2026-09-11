48447b2b2e5892ff8f494568f7ac220061c44390
Cozynxis/Levi <levi.jvbcollege@gmail.com>
2026-09-11 18:03:48 +0200

    Remove incomplete extension service worker

--- extension/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((message) => {
-  if (message?.type !== 'open-share') return;
-  const params = new URLSearchParams({
-    mode: message.mode === 'teacher' ? 'teacher' : 'student',
-    studentId: message.studentId || '',
-    studentName: message.studentName || ''
-  });
-  chrome.tabs.create({url: chrome.runtime.getURL(`share.html?${params.toString()}`)});
-});
